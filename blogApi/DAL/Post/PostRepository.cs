a2d522929c50e3ed3395d4c1c9a4569f915a305c
Anieze Collins <chigoziecollins434@gmail.com>
2019-11-29 10:27:10 +0100

    Created the post repositories

---
+++ blogApi/DAL/Post/PostRepository.cs
@@ -0,0 +1,27 @@
+﻿using blogApi.DTOS.ReadDTO;
+using blogApi.Entities;
+using blogApi.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace blogApi.DAL.Post
+{
+    public class PostRepository : BaseRepository<posts>, IPostRepository
+    {
+        private readonly RepositoryContext context;
+        public PostRepository(RepositoryContext _context) : base(_context)
+        {
+            context = _context;
+        }
+
+        public async Task<List<GetPostReadDTO>> GetPostsAsync()
+        {
+            var result = await context.GetPostReadDTOs.ToListAsync();
+                 
+            return result;
+        }
+    }
+}
