2f3946d8d2b774dd81caf930261fa2de65425ea7
Anieze Collins <chigoziecollins434@gmail.com>
2019-12-03 14:59:48 +0100

    Creatd the post replies write DTO

---
+++ blogApi/DTOS/WriteDTO/PostWriteUpdateDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace blogApi.DTOS.WriteDTO
+{
+    public class PostWriteUpdateDTO
+    {
+        [Required]
+        public int post_Id { get; set; }
+
+        [Required]
+        public string message { get; set; }
+    }
+}
