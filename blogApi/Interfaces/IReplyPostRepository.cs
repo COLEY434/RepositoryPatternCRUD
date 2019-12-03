1cf6328351815e7114e87ea7ebd086efaa14368a
Anieze Collins <chigoziecollins434@gmail.com>
2019-12-03 14:55:04 +0100

    Created the Reply repository interface

---
+++ blogApi/Interfaces/IReplyPostRepository.cs
@@ -0,0 +1,12 @@
+﻿using blogApi.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace blogApi.Interfaces
+{
+    public interface IReplyPostRepository : IBaseRepository<Replies>
+    {
+    }
+}
