728876e34227b18d94bc578c25db97620a6b3b4b
Anieze Collins <chigoziecollins434@gmail.com>
2019-12-03 14:59:23 +0100

    Created this read DTO

---
+++ blogApi/DTOS/ReadDTO/GetPostReadDTOs.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace blogApi.DTOS.ReadDTO
+{
+    public class GetPostReadDTOs
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string message { get; set; }
+
+        public string username { get; set; }
+
+        public string img_url { get; set; }
+
+        [Required]
+        public DateTime date_posted { get; set; }
+
+        public DateTime? date_updated { get; set; }
+
+        public int userId { get; set; }
+    }
+}
