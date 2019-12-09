5ef2e4232c4ff0d63611b221c5f8896b5ba2491a
Anieze Collins <chigoziecollins434@gmail.com>
2019-12-09 16:07:04 +0100

    changed the get user by id input parameter from long to int

--- blogApi/Interfaces/LoginManagement/IUsersRepository.cs
+++ blogApi/Interfaces/LoginManagement/IUsersRepository.cs
@@ -10,8 +10,8 @@ namespace blogApi.Interfaces.LoginManagement
     public interface IUsersRepository : IBaseRepository<users>
     {
         Task<IEnumerable<users>> GetAllUsersAsync();
-        Task<users> GetUserById(long id);
-        Task<users> GetUserByIdT(long id);
+        Task<UserReadDTO> GetUserById(int id);
+        Task<users> GetUserByIdT(int id);
         Task<IEnumerable<MaleUserDTO>> GetMaleUsers();
 
         Task<loginId> ValidateUser(string password, string email);
