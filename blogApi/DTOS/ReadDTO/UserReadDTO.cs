2e1e7a836987229c35d2904e048e3b3e198869a8
Anieze Collins <chigoziecollins434@gmail.com>
2019-12-09 16:07:57 +0100

    created the read DTO for displaying user data

---
+++ blogApi/DTOS/ReadDTO/UserReadDTO.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace blogApi.DTOS.ReadDTO
+{
+    public class UserReadDTO
+    {
+        public int userId { get; set; }
+
+        public string surname { get; set; }
+
+        public string firstname { get; set; }
+
+        public string username { get; set; }
+
+        public string gender { get; set; }
+
+        public string state { get; set; }
+
+        public string country { get; set; }
+
+        public int? age { get; set; }
+
+        public string email { get; set; }
+
+        public string img_url { get; set; }
+
+    }
+}
---
+++ blogApi/DTOS/WriteDTO/UserUpdateDTO.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace blogApi.DTOS.WriteDTO
+{
+    public class UserUpdateDTO
+    {
+        [Required]
+        public string Surname { get; set; }
+
+        [Required]
+        public string Firstname { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public int Age { get; set; }
+
+        [Required]
+        public string Gender { get; set; }
+
+        [Required]
+        public string State { get; set; }
+
+        [Required]
+        public string Country { get; set; }
+
+
+
+    }
+}
