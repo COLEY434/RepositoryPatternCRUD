35e4bda27ddcb23a2d4eb1ba8ee007ccdacc0f04
Anieze Collins <chigoziecollins434@gmail.com>
2020-01-03 23:59:28 +0100

    Created controller for authenticating and generating user token.

---
+++ blogApi/Controllers/AuthenticationController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using blogApi.DAL;
+using blogApi.DTOS.ReadDTO;
+using blogApi.DTOS.WriteDTO;
+using blogApi.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace blogApi.Controllers
+{
+    [Route("api/authenticate")]
+    [ApiController]
+    
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly IAuthenticationService _authService;
+        public AuthenticationController(IAuthenticationService authService)
+        {
+            _authService = authService;
+        }
+
+        [Route("login")]
+        [HttpPost]
+        public async Task<IActionResult> Authenticate([FromBody] AuthenticationRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var authResponse = await _authService.AuthenticateUserAsync(request.Email, request.Password);
+
+            if (!authResponse.Success)
+            {
+                return Ok(new AuthFailedResponse
+                {
+                    Success = authResponse.Success,
+                    ErrorMessage = authResponse.ErrorMesage
+                });
+            }
+
+            return Ok(new AuthSuccessResponse
+            {
+                Success = authResponse.Success,
+                UserId = authResponse.UserId,
+                ExpiresIn = authResponse.ExpiresIn,
+                Token = authResponse.Token,
+                Username = authResponse.Username
+            });
+        }
+
+        [Route("register")]
+        [HttpPost]
+        public async Task<IActionResult> Register([FromBody] AuthenticationRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var CheckIfUserExist = await _authService.FindUserAsync(request.Email, request.Password);
+
+            if (!CheckIfUserExist)
+            {
+                var authResponse = await _authService.RegisterAsync(request.Email, request.Password);
+
+                return Ok(authResponse);
+            }
+
+            return Ok(new AuthFailedResponse 
+            { 
+                Success = false,
+                ErrorMessage = "User with this email already exist"
+            });
+            
+        }
+    }
+}
