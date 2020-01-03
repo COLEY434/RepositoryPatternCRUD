9a67ea7a45a48ce746419dd6841996b91cc6ece2
Anieze Collins <chigoziecollins434@gmail.com>
2020-01-04 00:01:32 +0100

    Added read DTO's  for  showing success and failed responses for user authentication.

---
+++ blogApi/DTOS/ReadDTO/AuthFailedResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace blogApi.DTOS.ReadDTO
+{
+    public class AuthFailedResponse
+    {
+        public bool Success { get; set; }
+
+        public string ErrorMessage { get; set; }
+    }
+}
---
+++ blogApi/DTOS/ReadDTO/AuthenticationResult.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace blogApi.DTOS.ReadDTO
+{
+    public class AuthenticationResult
+    {
+        public int? UserId { get; set; }
+        public string Token { get; set; }
+
+        public DateTime? ExpiresIn { get; set; }
+
+        public bool Success { get; set; }
+
+        public string ErrorMesage { get; set; }
+
+        public string Username { get; set; }
+    }
+}
